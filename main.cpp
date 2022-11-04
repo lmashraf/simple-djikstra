c8549416fc91f8bfe0cd30f4db7ba5cf93c2cb4d
Mohammed Achraf LEBRAZI <lebrazi.med.achraf@gmail.com>
2022-11-04 18:04:48 +0100

    Added some execution examples on main.cpp

--- main.cpp
+++ main.cpp
@@ -7,20 +7,27 @@ int main()
             {
                     { 0, 1, 20 },
                     { 2, 3, 99 },
+                    { 2, 6, 200 },
                     { 3, 4, 35 },
                     { 1, 2, 10 },
                     { 1, 4, 150 },
-                    { 4, 0, 450 },
+                    { 4, 0, 450 }
             };
 
     // Initialise the Graph
-    int numberOfEdges = edges.size( ) - 1;
+    int numberOfEdges = edges.size( ) ;
+    std::cout << "Initial Number of Edges: " << numberOfEdges << std::endl;
     Graph graph( edges, numberOfEdges );
 
     // Add additional Edges/Nodes to the Graph
     graph.addEdge( { 4,5, 44 } ) ;
     graph.addEdge( { 5,6, 250 } );
     graph.addEdge( { 1,5, 20 } );
+    graph.addEdge( { 7,2, 220 } );
+
+    // View number of Edges
+    numberOfEdges = edges.size( ) ;
+    std::cout << "Current number of Edges: " << numberOfEdges << std::endl;
 
     // Print the Graph
     graph.printGraphBySourceNode( );
@@ -28,7 +35,7 @@ int main()
     graph.listAllNodes();
 
     // Search for the shortest path from sourceNode to destinationNode
-    std::cout << std::setw(64) << std::setfill('#') << "#"  << std::endl;
+    /*std::cout << std::setw(64) << std::setfill('#') << "#"  << std::endl;
     int sourceNode, destinationNode;
 
     std::cout << "Enter initial node:";
@@ -37,7 +44,14 @@ int main()
     std::cout << "Enter destination node:";
     std::cin >> destinationNode;
 
-    graph.shortestPath(sourceNode, destinationNode);
+    graph.shortestPath(sourceNode, destinationNode);*/
+
+    // Run Dijkstra's algorithm from every node
+    for (int sourceNode = 0; sourceNode <= numberOfEdges; ++sourceNode )
+    {
+        std::cout << std::setw(64) << std::setfill('#') << " ####### SrcNode: " << sourceNode << std::endl;
+        graph.shortestPath( sourceNode, numberOfEdges );
+    }
 
     return 0;
 }
